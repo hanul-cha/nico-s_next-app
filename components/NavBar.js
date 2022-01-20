d52cb0cab7dd7f9994c06d81ff1dc36fcb0fedcc
hanul-cha <ckgks3014@naver.com>
2022-01-20 14:16:26 +0900

    basic css

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,14 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import style from "./NavBar.module.css"
 
 const NavBar = () => {
+  const router = useRouter();
+
   return (
     <div>
       <Link href="/">
-        <a>home</a>
+        <a className={`${style.link}  ${router.pathname === "/" ? style.active : ""}`}>home</a>
       </Link>
       <Link href="/about">
-        <a>about</a>
+        <a className={[style.link, router.pathname === "/about" ? style.active : ""].join(" ")}>about</a>
       </Link>
     </div>
   );
